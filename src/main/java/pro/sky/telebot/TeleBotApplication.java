874e434b4d16051b04e0dcf6c125e85ee8480d03
Spolpinka <a421243266@gmail.com>
2023-06-30 06:21:53 +0300

    Initial commit

---
+++ src/main/java/pro/sky/telebot/TeleBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeleBotApplication.class, args);
+    }
+
+}
