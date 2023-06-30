14b3a3ef076e2d319a305f5d8bcb1057a66b7c18
Spolpinka <a421243266@gmail.com>
2023-06-30 08:12:44 +0300

    + /start + парсинг сообщений

--- src/main/java/pro/sky/telebot/listener/TelegramBotUpdateListener.java
+++ src/main/java/pro/sky/telebot/listener/TelegramBotUpdateListener.java
@@ -4,17 +4,30 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.Nullable;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class TelegramBotUpdateListener implements UpdatesListener {
+
+    private final Pattern pattern = Pattern.compile("(\\d{1,2}\\.\\d{1,2}\\.\\d{4} \\d{1,2}:\\d{2})\\s+" +
+            " ([А-я\\d\\s.,!?;:]+)");
+
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+            .ofPattern("dd:MM:yyyy HH:mm");
 
     Logger logger = LoggerFactory.getLogger(TelegramBotUpdateListener.class);
 
@@ -25,7 +38,7 @@ public class TelegramBotUpdateListener implements UpdatesListener {
     }
 
     @PostConstruct
-    public void init(){
+    public void init() {
         telegramBot.setUpdatesListener(this);
     }
 
@@ -33,7 +46,7 @@ public class TelegramBotUpdateListener implements UpdatesListener {
     public int process(List<Update> list) {
         try {
             list.forEach(update -> {
-                logger.info("Handles update: ", update.message().text());
+                logger.info("Handles update: ", update);
 
                 Message message = update.message();
 
@@ -41,13 +54,38 @@ public class TelegramBotUpdateListener implements UpdatesListener {
                 String text = message.text();
 
                 if ("/start".equals(text)) {
-                    SendMessage sendMessage = new SendMessage(id, "Hah, start... start by yourself");
-                    SendResponse sendResponse = telegramBot.execute(sendMessage);
-                    if (!sendResponse.isOk()) {
-                        logger.error("Error while sending message {}", sendResponse.description());
+                    sendMessage(id, """
+                            Hah, start
+                            ~maybe you'll do it by yourself~
+                            __or not__
+                            _или нет_
+                            *а ты попробуй*
+                            ||Fuck off||
+                            [github] \\(https://github\\.com/Spolpinka\\)
+                            """);
+
+                }
+                if ("/старт".equals(text)) {
+                    sendMessage(id, """
+                            _Значит, смотри\\!_
+                            здесь можно фиксировать себе напоминалки
+                            *для этого отправь сюда сообщение в формате:*
+                            __30\\.06\\.2023 23:00 \\{название своей задачи\\}__                         
+                            """);
+
+                }
+                if (text != null) {
+                    Matcher matcher = pattern.matcher(text);
+                    if (matcher.find()) {
+                        //обработка задания
+                        LocalDateTime dateTime = parseDateTime(matcher.group(1));
+                        String innerText = matcher.group(2);
+                    } else {
+                        //отбивка, что не соответствует шаблону
+                        sendMessage(id, "Не найден установленный формат сообщения!");
+
                     }
                 }
-
 
 
             });
@@ -56,7 +94,26 @@ public class TelegramBotUpdateListener implements UpdatesListener {
         }
 
 
-
         return CONFIRMED_UPDATES_ALL;
+    }
+
+    private void sendMessage(Long id, String message) {
+        SendMessage sendMessage = new SendMessage(id, message);
+        sendMessage.parseMode(ParseMode.MarkdownV2);
+        SendResponse sendResponse = telegramBot.execute(sendMessage);
+        if (!sendResponse.isOk()) {
+            logger.error("Error while sending message {}", sendResponse.description());
+        }
+
+    }
+
+    @Nullable
+    private LocalDateTime parseDateTime(String dateTime) {
+        try {
+            return LocalDateTime.parse(dateTime, dateTimeFormatter);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
