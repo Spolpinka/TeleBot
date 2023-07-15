d6cb1a9aead7815856e620753353d762bf909cbb
Spolpinka <a421243266@gmail.com>
2023-07-15 07:57:51 +0300

    + /start + парсинг сообщений + liquibase

--- src/main/java/pro/sky/telebot/model/NotificationTask.java
+++ src/main/java/pro/sky/telebot/model/NotificationTask.java
@@ -14,6 +14,7 @@ public class NotificationTask {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String message;
 
     @Column(name = "chat_id", nullable = false)
